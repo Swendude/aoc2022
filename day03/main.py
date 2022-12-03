d2b334961b9c6e27d551225b003c4918cff9169d
Swen Mulderij <swenmulderij@gmail.com>
2022-12-03 20:54:13 +0100

    After day 3 challenge 2 done

--- day03/main.py
+++ day03/main.py
@@ -1,11 +1,18 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+def groups(elves):
+    cur_elf = 0
+    while cur_elf < len(elves):
+        yield elves[cur_elf : cur_elf + 3]
+        cur_elf += 3
+
+
 with open("input.txt", "r") as inpfile:
     total = 0
-    for line in inpfile:
-        line = line.strip()
-        half = int(len(line) / 2)
-        first, second = line[half:], line[:half]
-        common = [c for c in first if c in second][0]
+    lines = inpfile.readlines()
+    for first, second, third in groups(lines):
+        common = [c for c in first if c in second and c in third][0]
         if common in alphabet:
             total += alphabet.index(common) + 1
         if common in alphabet.upper():
